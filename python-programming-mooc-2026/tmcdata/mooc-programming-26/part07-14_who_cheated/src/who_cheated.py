2e216588e5819df5ed6a19ab131aa92f0f3a7ffd
ni6hant <ni6hant@gmail.com>
2026-07-28 16:17:41 +0530

    one last question remain for the intro course

--- python-programming-mooc-2026/tmcdata/mooc-programming-26/part07-14_who_cheated/src/who_cheated.py
+++ python-programming-mooc-2026/tmcdata/mooc-programming-26/part07-14_who_cheated/src/who_cheated.py
@@ -1,1 +1,33 @@
 # Write your solution here
+import csv
+from datetime import timedelta, datetime
+
+
+def start_times_process(filename):
+    start_times_dict = {}
+    with open("start_times.csv") as my_file:
+        for line in csv.reader(my_file, delimiter=";"):
+            start_times_dict[line[0]] = datetime.strptime("1:"+line[1], "%d:%H:%M") #Default to day 1
+        return start_times_dict
+
+
+def submission_process(filename):
+    submission_list = []
+    with open(filename) as my_file:
+        for line in csv.reader(my_file, delimiter=";"):
+            # name;task;points;hh:mm
+            submission_list.append([line[0],int(line[1]),int(line[2]),datetime.strptime("1:"+line[3], "%d:%H:%M")])
+    return sorted(submission_list)
+
+
+def cheaters():
+    cheaters_list = []
+    start_data = start_times_process("start_times.csv")
+    submission_data = submission_process("submissions.csv")
+    for item in submission_data:
+        if item[3]-start_data[item[0]]>timedelta(hours=3) and item[0] not in cheaters_list:
+            cheaters_list.append(item[0])
+    return cheaters_list
+
+if __name__ == "__main__":
+    print(cheaters())
--- python-programming-mooc-2026/tmcdata/mooc-programming-26/part07-18_own_programming_language/src/own_language.py
+++ python-programming-mooc-2026/tmcdata/mooc-programming-26/part07-18_own_programming_language/src/own_language.py
@@ -1,1 +1,75 @@
 # Write your solution here
+#TODO: This long problem! It's still not done
+def run(execution_list:list):
+    for item in list:
+
+        pass
+    return 
+
+if __name__ == "__main__":
+    program1 = []
+    program1.append("MOV A 1")
+    program1.append("MOV B 2")
+    program1.append("PRINT A")
+    program1.append("PRINT B")
+    program1.append("ADD A B")
+    program1.append("PRINT A")
+    program1.append("END")
+    result = run(program1)
+    print(result)
+
+    program2 = []
+    program2.append("MOV A 1")
+    program2.append("MOV B 10")
+    program2.append("begin:")
+    program2.append("IF A >= B JUMP quit")
+    program2.append("PRINT A")
+    program2.append("PRINT B")
+    program2.append("ADD A 1")
+    program2.append("SUB B 1")
+    program2.append("JUMP begin")
+    program2.append("quit:")
+    program2.append("END")
+    result = run(program2)
+    print(result)
+
+    program3 = []
+    program3.append("MOV A 1")
+    program3.append("MOV B 1")
+    program3.append("begin:")
+    program3.append("PRINT A")
+    program3.append("ADD B 1")
+    program3.append("MUL A B")
+    program3.append("IF B <= 10 JUMP begin")
+    program3.append("END")
+    result = run(program3)
+    print(result)
+
+    program4 = []
+    program4.append("MOV N 50")
+    program4.append("PRINT 2")
+    program4.append("MOV A 3")
+    program4.append("begin:")
+    program4.append("MOV B 2")
+    program4.append("MOV Z 0")
+    program4.append("test:")
+    program4.append("MOV C B")
+    program4.append("new:")
+    program4.append("IF C == A JUMP error")
+    program4.append("IF C > A JUMP over")
+    program4.append("ADD C B")
+    program4.append("JUMP new")
+    program4.append("error:")
+    program4.append("MOV Z 1")
+    program4.append("JUMP over2")
+    program4.append("over:")
+    program4.append("ADD B 1")
+    program4.append("IF B < A JUMP test")
+    program4.append("over2:")
+    program4.append("IF Z == 1 JUMP over3")
+    program4.append("PRINT A")
+    program4.append("over3:")
+    program4.append("ADD A 1")
+    program4.append("IF A <= N JUMP begin")
+    result = run(program4)
+    print(result)
