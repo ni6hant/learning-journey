d8105baacc9ebf91978d61306a5e6f13657030c9
ni6hant <ni6hant@gmail.com>
2026-08-14 07:57:44 +0530

    13-Aug-2026

--- python-programming-mooc-2026/tmcdata/mooc-programming-26/part08-13_stopwatch/src/stopwatch.py
+++ python-programming-mooc-2026/tmcdata/mooc-programming-26/part08-13_stopwatch/src/stopwatch.py
@@ -3,3 +3,31 @@ class Stopwatch:
     def __init__(self):
         self.seconds = 0
         self.minutes = 0
+
+    def __str__(self):
+        # if self.seconds<=9:
+        #     seconds = "0" + str(self.seconds)
+        # else:
+        #     seconds = self.seconds
+        # if self.minutes<=9:
+        #     minutes = "0" + str(self.minutes)
+        # else:
+        #     minutes = self.minutes
+        # return f"{minutes}:{seconds}"
+        return f"{self.minutes:02}:{self.seconds:02}"
+
+    def tick(self):
+        if self.seconds==59:
+            self.minutes+=1
+            self.seconds=0
+        else:
+            self.seconds+=1
+
+        if self.minutes==60:
+            self.minutes=0
+
+if __name__ == "__main__":
+    watch = Stopwatch()
+    for i in range(500):
+        print(watch)
+        watch.tick()
