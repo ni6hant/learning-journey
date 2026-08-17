2e64c3e0b29ab247a058f1b75a068c93f1428ec0
ni6hant <ni6hant@gmail.com>
2026-08-17 11:50:24 +0530

    .

--- python-programming-mooc-2026/tmcdata/mooc-programming-26/part09-01_fastest_car/src/fastest_car.py
+++ python-programming-mooc-2026/tmcdata/mooc-programming-26/part09-01_fastest_car/src/fastest_car.py
@@ -5,7 +5,27 @@ class Car:
         self.make = make
         self.top_speed = top_speed
 
-   def __str__(self):
+    def __str__(self):
         return f"Car (make: {self.make}, top speed: {self.top_speed})"
 
 # WRITE YOUR SOLUTION HERE:
+def fastest_car(cars:list):
+    #Default Value Stored
+    fastest_make = cars[0].make
+    fastest_top_speed = cars[0].top_speed
+    for car in cars:
+        if car.top_speed>fastest_top_speed:
+            fastest_make = car.make
+            fastest_top_speed = car.top_speed
+    return fastest_make
+
+
+
+if __name__ == "__main__":
+    car1 = Car("Saab", 195)
+    car2 = Car("Lada", 110)
+    car3 = Car("Ferrari", 280)
+    car4 = Car("Trabant", 85)
+
+    cars = [car1, car2, car3, car4]
+    print(fastest_car(cars))
