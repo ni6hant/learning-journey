3a9881eedb3cb367f4f1e819d9383dc2081c5975
ni6hant <ni6hant@gmail.com>
2026-08-19 10:52:08 +0530

    18 Aug 2026

--- Python-Journey/python-programming-mooc-2026/mooc-programming-26/part09-07_box_of_presents/src/box_of_presents.py
+++ Python-Journey/python-programming-mooc-2026/mooc-programming-26/part09-07_box_of_presents/src/box_of_presents.py
@@ -1,1 +1,21 @@
 # WRITE YOUR SOLUTION HERE:
+class Present:
+    def __str__(self):
+        return f"{self.name} ({self.weight} kg)"
+    
+    def __init__(self, name:str, weight: int):
+        self.name = name
+        self.weight = weight
+
+class Box:
+    def __init__(self):
+        self.total_weight_of_box = 0
+        self.contents = []
+        pass
+
+    def add_present(self, present: Present):
+        self.contents.append(present)
+        self.total_weight_of_box += present.weight
+
+    def total_weight(self):
+        return self.total_weight_of_box
