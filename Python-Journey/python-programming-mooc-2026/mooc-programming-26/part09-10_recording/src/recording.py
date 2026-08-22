fa4d5b774bc1db80870eb6dee5033af12a5fa806
ni6hant <ni6hant@gmail.com>
2026-08-22 11:38:33 +0530

    21aug

--- Python-Journey/python-programming-mooc-2026/mooc-programming-26/part09-10_recording/src/recording.py
+++ Python-Journey/python-programming-mooc-2026/mooc-programming-26/part09-10_recording/src/recording.py
@@ -1,1 +1,24 @@
 # WRITE YOUR SOLUTION HERE:
+class Recording:
+    def __init__(self,length):
+        if length>=0:
+            self.__length = length
+        else:
+            raise ValueError("Length should be more than zero")
+
+    @property
+    def length(self):
+        return self.__length
+
+    @length.setter
+    def length(self,length:int):
+        if length>=0:
+            self.__length = length
+        else:
+            raise ValueError("Length should be more than zero")
+
+if __name__ == "__main__":
+    the_wall = Recording(-1)
+    print(the_wall.length)
+    the_wall.length = 44
+    print(the_wall.length)
--- Python-Journey/python-programming-mooc-2026/mooc-programming-26/part09-11_weather_station/src/weather_station.py
+++ Python-Journey/python-programming-mooc-2026/mooc-programming-26/part09-11_weather_station/src/weather_station.py
@@ -1,3 +1,34 @@
 # WRITE YOUR SOLUTION HERE:
+class WeatherStation:
+    def __init__(self, name:str):
+        self.__name = name
+        self.__observations = []
 
+    def __str__(self):
+        return f"{self.__name}, {len(self.__observations)} observations"
 
+    # @property
+    # def name(self):
+    #     return self.__name
+
+    # @name.setter
+    # def name(self,name):
+    #     if name != "":
+    #         self._name = _name
+    #     else:
+    #         raise ValueError("The weather name shouldn't be empty")
+    
+    def add_observation(self,observation:str):
+        if observation != "":
+            self.__observations.append(observation)
+        else:
+            raise ValueError("The observation name shouldn't be empty")
+
+    def latest_observation(self):
+        if len(self.__observations) == 0:
+            return f"No observations in {self.__name} yet"
+        else:
+            return self.__observations[-1]
+    
+    def number_of_observations(self):
+        return len(self.__observations)
