aeefb4af493d54d1d66bcaee4a5d7f2f940a813b
ni6hant <ni6hant@gmail.com>
2026-08-24 20:54:47 +0530

    24-Aug-2026

--- Python-Journey/python-programming-mooc-2026/mooc-programming-26/part09-12_service_charge/src/service_charge.py
+++ Python-Journey/python-programming-mooc-2026/mooc-programming-26/part09-12_service_charge/src/service_charge.py
@@ -1,3 +1,27 @@
 # WRITE YOUR SOLUTION HERE:
+class BankAccount:
+    def __init__(self,owner:str, account_number:str, balance:float):
+        self.__owner = owner
+        self.__account_number = account_number
+        self.__balance = balance
 
+    @property
+    def balance(self):
+        return self.__balance
 
+    def deposit(self,amount: float):
+        if amount>0:
+            self.__balance+=amount
+            self.__service_charge()
+        else:
+            raise ValueError("Amount depositing has to be more than zero.")
+
+    def withdraw(self,amount: float):
+        if amount>0:
+            self.__balance-=amount
+            self.__service_charge()
+        else:
+            raise ValueError("Amount depositing has to be more than zero.")
+
+    def __service_charge(self):
+        self.__balance = self.__balance*(1 - 1/100)
