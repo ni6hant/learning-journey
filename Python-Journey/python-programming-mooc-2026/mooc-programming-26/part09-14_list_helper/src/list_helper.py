d1aae8187a6ee2e46c65ed59acca52e11777b0ec
ni6hant <ni6hant@gmail.com>
2026-08-29 21:05:51 +0530

    29-Aug-2026

--- Python-Journey/python-programming-mooc-2026/mooc-programming-26/part09-14_list_helper/src/list_helper.py
+++ Python-Journey/python-programming-mooc-2026/mooc-programming-26/part09-14_list_helper/src/list_helper.py
@@ -1,1 +1,25 @@
 # WRITE YOUR SOLUTION HERE:
+class ListHelper:
+
+    @classmethod
+    def greatest_frequency(cls,my_list: list):
+        if not my_list:
+            return None
+
+        frequency_dict = {}
+        for item in my_list:
+            frequency_dict[item] = my_list.count(item)
+        return max(frequency_dict,key=frequency_dict.get)
+
+    @classmethod
+    def doubles(cls,my_list: list):
+        unique_two = []
+        for item in my_list:
+            if my_list.count(item)>=2 and item not in unique_two:
+                unique_two.append(item)
+        return len(unique_two)
+
+if __name__ == "__main__":
+    numbers = [1, 1, 2, 1, 3, 3, 4, 5, 5, 5, 6, 5, 5, 5]
+    print(ListHelper.greatest_frequency(numbers))
+    print(ListHelper.doubles(numbers))
